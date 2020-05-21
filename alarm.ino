9ddee276f06bf193ccf9407dbc7da4f5253a76cd
jaenek <jaenek@protonmail.com>
2020-05-21 14:07:59 +0200

    Add displaying current time, cleanup

--- alarm.ino
+++ alarm.ino
@@ -1,12 +1,18 @@
 #include <Arduino.h>
+#include <NTPClient.h>
 #include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
 #include <AudioFileSourceHTTPStream.h>
 #include <AudioGeneratorMP3.h>
 #include <AudioOutputI2S.h>
+#include <Wire.h>
+#include "SSD1306Wire.h"
 
-// To run, set your ESP8266 build to 160MHz, update the SSID info, and upload.
+// NTP setup
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "europe.pool.ntp.org", 7200);
 
-// Enter your WiFi setup here:
+// Wifi setup
 #include "alarm.h"
 #ifndef STASSID
 #define STASSID "SSID"
@@ -16,35 +22,58 @@
 const char* ssid = STASSID;
 const char* password = STAPSK;
 
-// Randomly picked URL
+// Audio setup
 const char *URL="http://tytus.dom:8000/";
 
 AudioGeneratorMP3 *mp3;
 AudioFileSourceHTTPStream *stream;
 AudioOutputI2S *out;
 
+// SSD1306 128x32 setup
+SSD1306Wire display(0x3c, D3, D5, GEOMETRY_128_32);
+
+void printtime()
+{
+	display.clear();
+	char time[10];
+	sprintf(time, "%02d:%02d",
+		timeClient.getHours(),
+		timeClient.getMinutes()
+	);
+	display.drawString(64, 16, String(time));
+	display.display();
+}
+
 void setup()
 {
 	Serial.begin(115200);
-	delay(1000);
-	Serial.println("Connecting to WiFi");
 
+	// Init display
+	display.init();
+	display.setTextAlignment(TEXT_ALIGN_CENTER_BOTH);
+	display.setFont(ArialMT_Plain_24);
+
+	// Establish WiFi connection
 	WiFi.disconnect();
 	WiFi.softAPdisconnect(true);
 	WiFi.mode(WIFI_STA);
 
 	WiFi.begin(ssid, password);
 
-	// Try forever
+	Serial.println("Connecting to WiFi");
+
 	while (WiFi.status() != WL_CONNECTED) {
-		Serial.println("...Connecting to WiFi");
+		Serial.print(".");
 		delay(1000);
 	}
-	Serial.println("Connected");
+	Serial.printf("\rConnected to %s", STASSID);
 
-	audioLogger = &Serial;
+	// Connect to NTP server
+	timeClient.begin();
+	timeClient.update();
+	printtime();
+
 	stream = new AudioFileSourceHTTPStream(URL);
-	stream->RegisterMetadataCB(MDCallback, (void*)"HTTP");
 	out = new AudioOutputI2S();
 	mp3 = new AudioGeneratorMP3();
 	mp3->begin(stream, out);
@@ -55,15 +84,12 @@ void loop()
 {
 	static int lastms = 0;
 
-	if (mp3->isRunning()) {
-		if (millis()-lastms > 1000) {
-			lastms = millis();
-			Serial.printf("Running for %d ms...\n", lastms);
-			Serial.flush();
-		}
-		if (!mp3->loop()) {
-			mp3->stop();
-			Serial.printf("MP3 done\n");
-		}
+	if (millis()-lastms >= 60000) {
+		printtime();
+		lastms = millis();
+	}
+
+	if (!mp3->loop()) {
+		mp3->stop();
 	}
 }
